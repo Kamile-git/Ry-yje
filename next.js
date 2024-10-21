4bffca02873244c8815c4b4469d7cb9e3fadc23f
Kamile-git <kamile.pociutee@gmail.com>
2024-10-21 10:22:21 +0300

    +js patobulinimai

--- next.js
+++ next.js
@@ -4,37 +4,49 @@
 
 
 function LaunchList() {
+    // Get query string from url
+    var loc = window.location.toString();
+    var queryString = loc.split('?')[1];
+  
+    // Get all the forms with .launchlist-form class
+    let forms = document.querySelectorAll(".launchlist-form");
+  
+    // Update the form with referral id or query params
+    forms.forEach(form => {
+      // Get the action url of the form
+      let action = form.getAttribute("action");
+  
+      // Check if query string is present
+      if (queryString) {
+        let queryParams = "?" + queryString;
+  
+        // Append query params to action URL only if they aren't already present
+        if (!action.includes(queryParams)) {
+          form.setAttribute("action", action + queryParams);
+        }
+      } else {
+        form.setAttribute("action", action);
+      }
+    });
+  }
+  
+  LaunchList();
 
-//     // Get query string from url
-var loc = window.location.toString();
-queryString = loc.split('?')[1];
 
-//     // Get all the forms with .launchlist-form class
-let forms = document.querySelectorAll(".launchlist-form");
 
-//     // Update the form with referral id
-forms.forEach(form => {
 
-//         // Get the action url of the form
-let action = form.getAttribute("action");
 
-//         // Check if query string is present
-if (
-queryString !== null &&
-queryString !== undefined &&
-queryString !== ""
-) {
-
-queryParams = "?" + queryString;
-//             // update action url with query params
-form.setAttribute("action", action + queryParams);
-}
-else {
-//             // update action url without query params
-form.setAttribute("action", action);
-}
-});
-
-}
-
-LaunchList();
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+  
+  document.querySelectorAll('.launchlist-form').forEach(form => {
+    form.addEventListener('submit', function(event) {
+      const emailInput = form.querySelector('input[name="email"]');
+      if (emailInput && !validateEmail(emailInput.value)) {
+        alert('Please enter a valid email address');
+        event.preventDefault(); // Stop form submission
+      }
+    });
+  });
